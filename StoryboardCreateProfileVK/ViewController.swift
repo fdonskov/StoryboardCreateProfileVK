f906cb7fa4a122ec8a7178039fd60423d306c387
Fedor Donskov <fedya_donskov@mail.ru>
2022-05-22 08:12:22 +0300

    Initial Commit

---
+++ StoryboardCreateProfileVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardCreateProfileVK
+//
+//  Created by Федор Донсков on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
